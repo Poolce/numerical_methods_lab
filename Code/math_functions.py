b102a5ee7a51568128eaf39736ba8fa3b458ebaa
Poolce <poolce@list.ru>
2022-11-26 17:16:11 +0300

    master

---
+++ Code/math_functions.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math
+
+def sin(x):
+    return np.sin(x)
+def x_11_12(x):
+    return 12*math.pow(x,11)
+def cos_2(x):
+    return np.square(np.cos(x))
+def x_2_3(x):
+    return np.square(x)*3
+def x_3_8(x):
+    return np.power(x,3)*8
+def x_4_5(x):
+    return np.power(x,4)*5
